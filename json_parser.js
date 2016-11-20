0dee7efaffa000335bbca20af942b5fe2fd556bb
Joachim <joachim.huet@epfl.ch>
2016-11-20 01:42:12 +0100

    Create exportable function and no more in test mode

--- json_parser.js
+++ json_parser.js
@@ -3,6 +3,43 @@
  * 
  */
  
+ /**
+  * Functions that can be used in other Node.js
+  */
+ module.exports = {
+	/**
+	 * Will display the playlists in the div with given id (@divID)
+	 */
+	displayPlaylists: function (jsonQueryPlaylists, divID) {
+		playlists = simplifyPlaylistJSON(jsonQueryPlaylists);
+
+		var items = playlists['items'];
+		
+		var htmlCode = "<ul>"
+		items.forEach(function(item) {
+			htmlCode += "<li><p><img src=\"" + item['image'] + "\" width=\"50\" height\"50\" /> " + item['name'] + "</p></li>"
+		});
+		htmlCode += "</ul>"
+		document.getElementById(divID).innerHTML = htmlCode;
+	},
+	
+	/**
+	 * Will display the tracks in the div with given id (@divID)
+	 */
+	displayTracks: function (jsonQueryTracks, divID) {
+		tracks = simplifyTracksJSON(jsonQueryTracks);
+		
+		var items = playlists['items'];
+		
+		var htmlCode = "<ul>"
+		items.forEach(function(item) {
+			htmlCode += "<li><p><img src=\"" + item['image'] + "\" width=\"50\" height\"50\" /> " + item['name'] + "</p></li>"
+		});
+		htmlCode += "</ul>"
+		document.getElementById(divID).innerHTML = htmlCode;
+	}
+};
+ 
 /**
  * This method will simplify the playlists json to keep only what we use
  * 
@@ -19,18 +56,15 @@
  * }
  */
 function simplifyPlaylistJSON (playlists) {
-	//document.body.appendChild(document.createTextNode("Voilà"));
-	//document.write("TEST");
 	var relevantJSON;
-	// TODO : Only now for testing phase
-	var jsonContent = readGivenFile('playlists.json');
-	/*
+	var jsonContent;
+	
 	try {
 		jsonContent = JSON.parse(playlists);
 	} catch (err) {
 		jsonContent = playlists;
 	}
-	*/
+	
 	
 	// We create a new json file only with relevant informations
 	var nOfPlaylists = Math.min(jsonContent['limit'], jsonContent['total']);
@@ -218,97 +252,6 @@ function readGivenFile(filename) {
   "total" : 2
 };
 	
-	/*
-	document.write("I start to read");
-	try {
-		fs.readFile(filename, 'utf-8', function read(err, data) {
-			if (err) {
-				document.write(err);
-				throw err;
-			}
-			content = data;		
-		});
-	} catch(err) {
-		document.write(err);
-	}
-	*/
-	
 	return content;
 }
- 
- 
-/**
- * Just a test method
- */
-function test() {
-	var fs = require('fs');
-	fs.readFile('testTracks.json', 'utf-8', function read(err, data) {
-		if (err) {
-			throw err;
-		}
-		content = data;
-
-		// Invoke the next step here however you like
-		//console.log(content);   // Put all of the code here (not the best solution)
-		processFile();          // Or put the next step in a function and invoke it
-	});
-
-	function processFile() {
-		// Get json file
-		var jsonContent = JSON.parse(content);
-		
-		// We create a new json file only with relevant informations
-		var nOfTracks = Math.min(jsonContent['limit'], jsonContent['total']);
-		var myTracks = [];
-		
-		// We go over all tracks one by one and keep only what is needed
-		for (i = 0; i < nOfTracks; i++) {
-			var currentItem = jsonContent['items'][i];
-			
-			var artists = currentItem["track"]["artists"];
-			var artistsNames = "";
-			
-			artists.forEach(function(artist) {
-				artistsNames += ' - ' + artist["name"];
-			});
-		
-			// Removes the first ' - ' 
-			artistsNames = artistsNames.substr(3);
-			
-			var cover;
-			
-			if (currentItem["track"]["album"]["images"].length > 1) {
-				cover = currentItem["track"]["album"]["images"][1];
-			} else {
-				cover = currentItem["track"]["album"]["images"][0];
-			}
-			
-			var newTrack = {
-				"artist_name" : artistsNames, 
-				"album_name" : currentItem["track"]["album"]["name"],
-				"track_name" : currentItem["track"]["name"], 
-				"cover_url" : cover,
-				"mp3_preview" : currentItem["track"]["preview_url"],
-				"track_url" : currentItem["track"]["external_urls"]["spotify"]
-				};
-				
-			myTracks.push(newTrack);
-		}
-		
-		relevantJSON = {
-			'items' : myTracks,
-			'next' : jsonContent['next'],
-			'size' : nOfTracks
-			};
-		
-		var i = 3;
-		
-		console.log(relevantJSON.items[i].artist_name);
-		console.log(relevantJSON.items[i].track_url);
-		console.log(relevantJSON.items[i].mp3_preview);
-	}
-}
-
-
-
 
